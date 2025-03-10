15438b0f4a1cddf48284602ff6d7939f77356aef
Kudhan <kudhanshaik01.com>
2025-03-10 12:07:47 +0530

    setting up the profile section

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -80,6 +80,20 @@ export const useAuthStore = create((set, get) => ({
       const errorMessage = error?.response?.data?.message || "An error occurred during logout";
       toast.error(errorMessage);
     }
-  }
+  },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
   
 }));
